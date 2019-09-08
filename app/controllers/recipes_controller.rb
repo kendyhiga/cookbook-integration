6488e0a26a5a0a0d5127b8514c92e47b5d79c6ff
kendyhiga <38764892+kendyhiga@users.noreply.github.com>
2019-09-08 16:20:46 -0300

    WIP: trying to make it work

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,21 @@ class RecipesController < ApplicationController
   end
 
   def create
+    uri = URI('http://localhost:3000/api/v1/recipes')
+    @response = Net::HTTP.post_form(uri, 'title' => 'Bala',
+                                         'difficulty' => 'Fácil',
+                                         'recipe_type_id' => '1',
+                                         'cuisine' => '1',
+                                         'cook_time' => '50',
+                                         'ingredients' => 'muitos',
+                                         'cook_method' => 'cozinhe tudo'
+                                         )
     byebug
-    @recipe.save
+    redirect_to new_recipe_path
+  end
+
+  def show
+    uri = URI('http://localhost:3000/api/v1/recipes/1')
+    @example = Net::HTTP.get(uri)
   end
 end
