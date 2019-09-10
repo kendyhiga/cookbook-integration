dbeb3d9bdc57390aba2781784d595311b30ddf50
kendyhiga <38764892+kendyhiga@users.noreply.github.com>
2019-09-09 22:48:05 -0300

    WIP: still working on it

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,21 +4,29 @@ class RecipesController < ApplicationController
   end
 
   def create
+    @recipe = Recipe.new(params[:recipe]['title'],
+                         params[:recipe]['recipe_type_id'],
+                         params[:recipe]['cuisine_id'],
+                         params[:recipe]['difficulty'],
+                         params[:recipe]['cook_time'],
+                         params[:recipe]['ingredients'],
+                         params[:recipe]['cook_method'])
     uri = URI('http://localhost:3000/api/v1/recipes')
-    @response = Net::HTTP.post_form(uri, 'title' => 'Bala',
-                                         'difficulty' => 'Fácil',
-                                         'recipe_type_id' => '1',
-                                         'cuisine' => '1',
-                                         'cook_time' => '50',
-                                         'ingredients' => 'muitos',
-                                         'cook_method' => 'cozinhe tudo'
-                                         )
+    json = { recipe: @recipe.instance_values }
     byebug
+    # json = { recipe: @recipe.instance_values.symbolize_keys }
+    @response = Net::HTTP.post_form(uri, json)
     redirect_to new_recipe_path
   end
 
   def show
     uri = URI('http://localhost:3000/api/v1/recipes/1')
     @example = Net::HTTP.get(uri)
+  end
+
+  private
+
+  def recipe_params
+     
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,6 +5,17 @@ class Recipe
   attr_accessor :title, :recipe_type_id, :cuisine_id, :difficulty,
                 :cook_time, :ingredients, :cook_method
 
+  def initialize(title = nil, recipe_type_id = nil, cuisine_id = nil, difficulty = nil,
+                 cook_time = nil, ingredients = nil, cook_method = nil)
+    @title = title
+    @recipe_type_id = recipe_type_id
+    @cuisine_id = cuisine_id
+    @difficulty = difficulty
+    @cook_time = cook_time
+    @ingredients = ingredients
+    @cook_method = cook_method
+  end
+
   def persisted?
     false
   end
