0cb9b6ddbc5f20c58424da837c78854da338af9b
kendyhiga <38764892+kendyhiga@users.noreply.github.com>
2019-09-11 00:27:46 -0300

    using Faraday to register new recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,40 +3,23 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
-  
+  def create
+    @recipe = Recipe.new(params[:recipe]['title'],
+                         params[:recipe]['recipe_type_id'],
+                         params[:recipe]['cuisine_id'],
+                         params[:recipe]['difficulty'],
+                         params[:recipe]['cook_time'],
+                         params[:recipe]['ingredients'],
+                         params[:recipe]['cook_method'],
+                         params[:recipe]['user_id'])
 
+    url = 'http://localhost:3000/api/v1/recipes'
 
-  # net/http attempt
-  # def create
-  #   @recipe = Recipe.new(params[:recipe]['title'],
-  #                        params[:recipe]['recipe_type_id'],
-  #                        params[:recipe]['cuisine_id'],
-  #                        params[:recipe]['difficulty'],
-  #                        params[:recipe]['cook_time'],
-  #                        params[:recipe]['ingredients'],
-  #                        params[:recipe]['cook_method'],
-  #                        params[:recipe]['user_id'])
-  #   uri = URI('http://localhost:3000/api/v1/recipes')
-  #   byebug
-  #   # json = { 'recipe' => @recipe.instance_values }
-  #   # json = { recipe: @recipe.instance_values.symbolize_keys }
-  #   # json = @recipe.to_json
-  #   # json = {'recipe' => @recipe.instance_values}.to_json
+    resp = Faraday.post(url) do |req|
+      req.headers['Content-Type'] = 'application/json'
+      req.body = {recipe: @recipe.instance_values }.to_json
+    end
 
-  #   json = @recipe.instance_values
-  #   json = { 'recipe' => json }
-  #   @response = Net::HTTP.post_form(uri, json)
-  #   redirect_to new_recipe_path
-  # end
+  end
 
-  # def show
-  #   uri = URI('http://localhost:3000/api/v1/recipes/1')
-  #   @example = Net::HTTP.get(uri)
-  # end
-
-  # private
-
-  # def recipe_params
-     
-  # end
 end
