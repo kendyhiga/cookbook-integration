794c91b2f644db806309a8dc35218a0f3c13b575
kendyhiga <38764892+kendyhiga@users.noreply.github.com>
2019-09-10 23:19:59 -0300

    WIP: last attempt to use nethttp

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,26 +3,36 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
 
-  def create
-    @recipe = Recipe.new(params[:recipe]['title'],
-                         params[:recipe]['recipe_type_id'],
-                         params[:recipe]['cuisine_id'],
-                         params[:recipe]['difficulty'],
-                         params[:recipe]['cook_time'],
-                         params[:recipe]['ingredients'],
-                         params[:recipe]['cook_method'],
-                         params[:recipe]['user_id'])
-    uri = URI('http://localhost:3000/api/v1/recipes')
-    json = { 'recipe' => @recipe.instance_values }
-    # json = { recipe: @recipe.instance_values.symbolize_keys }
-    @response = Net::HTTP.post_form(uri, json)
-    redirect_to new_recipe_path
-  end
+  
 
-  def show
-    uri = URI('http://localhost:3000/api/v1/recipes/1')
-    @example = Net::HTTP.get(uri)
-  end
+
+  # net/http attempt
+  # def create
+  #   @recipe = Recipe.new(params[:recipe]['title'],
+  #                        params[:recipe]['recipe_type_id'],
+  #                        params[:recipe]['cuisine_id'],
+  #                        params[:recipe]['difficulty'],
+  #                        params[:recipe]['cook_time'],
+  #                        params[:recipe]['ingredients'],
+  #                        params[:recipe]['cook_method'],
+  #                        params[:recipe]['user_id'])
+  #   uri = URI('http://localhost:3000/api/v1/recipes')
+  #   byebug
+  #   # json = { 'recipe' => @recipe.instance_values }
+  #   # json = { recipe: @recipe.instance_values.symbolize_keys }
+  #   # json = @recipe.to_json
+  #   # json = {'recipe' => @recipe.instance_values}.to_json
+
+  #   json = @recipe.instance_values
+  #   json = { 'recipe' => json }
+  #   @response = Net::HTTP.post_form(uri, json)
+  #   redirect_to new_recipe_path
+  # end
+
+  # def show
+  #   uri = URI('http://localhost:3000/api/v1/recipes/1')
+  #   @example = Net::HTTP.get(uri)
+  # end
 
   # private
 
